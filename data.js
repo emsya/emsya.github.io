cd9527d75d520846114096032893a08308672ea9
EMSYA Rep <muhamadsyae543@gmail.com>
2023-10-28 02:43:52 +0700

    emsya
    28okt

---
+++ data.js
@@ -0,0 +1,1 @@
+navbar = '[{"menu1" : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia rerum quaerat ratione excepturi vel expedita harum repellendus in, atque optio, eveniet sint sequi deserunt minima? Accusamus autem in ratione repellat."}, {"title2" : "Voucher"}]';

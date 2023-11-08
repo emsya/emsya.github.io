7390679b8b2c473c6efde5b3ee2c883a6f64a578
EMSYA Rep <muhamadsyae543@gmail.com>
2023-11-08 21:52:12 +0700

    08nov

--- menu.js
+++ menu.js
@@ -34,6 +34,9 @@ menu.innerHTML = `
                         <li class="nav-item">
                             <a class="nav-link text-light-emphasis" href="qrcode.html">QR-Code</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light-emphasis" href="qrcode.html">Generator</a>
+                        </li>
                     </ul>
                     <form class="d-flex" role="search">
                         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">

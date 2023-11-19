830f4a55284e6693bee741a91c7316c42d62a96f
EMSYA Rep <muhamadsyae543@gmail.com>
2023-11-19 21:55:38 +0700

    sdb

---
+++ recent.js
@@ -0,0 +1,28 @@
+let sidebar = document.getElementById('sidebar');
+
+sidebar.innerHTML = `
+<div class="sidebar ">
+                  <!-- sidebar -->
+                  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2189401836258981"
+     crossorigin="anonymous"></script>
+<!-- emsya ads -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2189401836258981"
+     data-ad-slot="2799359085"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+                  <h4 class="rpo">🔻Recent Post</h4>
+                 <ul class="list-sidebar">
+                    <li><a href="template.html">Template Login Page Hotspot</a></li>
+                    <li><a href="qrcode.html">Tutorial Pasang Qr-Code Scanner</a></li>
+                    <li><a href="template.html">Template Login Page Hotspot</a></li>
+                    <li><a href="template.html">Template Login Page Hotspot</a></li>
+                    <li><a href="template.html">Template Login Page Hotspot</a></li>
+                 </ul>
+
+                   </div>
+`;
